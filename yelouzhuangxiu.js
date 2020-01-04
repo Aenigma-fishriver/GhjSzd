f6ed98f69dafb13bd156236425b1eb2e9e93b40d
Aenigma-fishriver <57610861+Aenigma-fishriver@users.noreply.github.com>
2020-01-04 14:35:03 +0800

    Create yelouzhuangxiu.js

---
+++ yelouzhuangxiu.js
@@ -0,0 +1,184 @@
+/* ..                                                              
+                              .    .@@@@@@@@@@@@@@@@@@@@@@@O.    .                        
+                             =@@@@@@@OOOOOOOOOOOOOOOOOOOOOO@@@@@@@.                       
+                       @@@@@@@OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO@@@@^                     
+                   @@@@@OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO@@@@@@O                 
+                       @@@@@@@OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO@@@@             
+                    .@@OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO@@.          
+                   @@OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO@@@@OO@@.        
+                .@@OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO@@@@@@^      
+              =@@OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO@@@@`..      
+              =@@OOOOOOOOOOOOOOOOOOOOOOOO@@@@@@OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO@@.        
+            .]/@@OOOOOOOOOOOOOOOOOOOOOOO@@@@@@@OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO@@]]`      
+        ,[\@@@@OOOOOOOOOOOOOOOOOOOOOOO@@@@OOOOOOOOOOOO@@@@OOOOOOOOOOOOOOOOOOOOOOO@@@      
+          =@@OOOOOOOOOOOOOOOOOOOOOOOOO@@@OOOOOOOOOOOOOOOOO@@OOOOOOOOOOOOOOOOOOOOO@@^      
+          =@@OOOOOOOOOOOOOOOOOOOOOOO@@OOOOOOOOOOOOOOOOOOOOOO@@OOOOOOOOOOOOOOOOOOO@@^      
+ ..   @@@@@OOOO@@OOOOOOOOOOOOOOOOOOO@@OOOOOOOOOOOOOOO@@@@@@@@@OOOOOOOOOOOOOOOOOOOOO@@^    
+ .@@@@OOOOOO@@@OOOOOOOOOOOOOOOOOOOOO@@OOOOOOOOOOOOO@@@OOOO@@@@OOOO@@@OOOOOOOOOOOOOO@@^    
+   .@@@@@@@@@OOOOOOOOOOOOOOOOOOOOO@O@@OOOOOOOOOOOOO@@@@@@@@@O@@@OOOOO@@OOOOOOOOOOOO@@^    
+          =@@OOOOOOOOOOOOOOOOOOOOO@@@@OOOOOOOOOOO@@`.......=@@@@@@OOOOO@@OOOOOOOOOO@@^    
+      .. .=@@OOOOOOOOOOOOOOOOOOO@@..@@OOOOOOOOOOO@@^............@@OOOOOOO@@OOOOOOOO@@^    
+       ...=@@OOOOOOOOOOOOOOOOOOO@@..@@OOOOOOOOOOO@@`............@@OOOOOOO@@OOOOOO@@@@^    
+      .. .=@@OOOOOOOOOOOOOOOOO@@....@@OOOOO@@@@OO@@^..............@@@OOOO@@@@OOOO@@@@@@O  
+          =@@OOOO@@OOOOOOOO@@@[[....@@@@@@@@@@@OO@@`..............@@@OOO@@@O@OOOO@@@@@@@@^
+        /OO@@OO@@@@OO@@OOOO@@@/OOO]]@@@@/[[[[@@OO@@^...,]]]]]]]]OO@@@OOO@@@@@OOOO@@/[[[[[`
+        @@@OOO@@@@@@@@@OO@@/[\@@@@@@@@/[`....@@@@[[....,[[@@@@@@[[[[\@O@@@@@@OOOO@@^      
+        @@@OO@@@@@@@@OO@@@@O.=@@@@@@@@^......@@@@........,@@@@@@`...=@@@@@@@@@@OO@@^      
+        @@@@@O@@@..@@@O@@O.=@@@@@@@@@@^......@@@@......=@@@@@@@@@@..=@@@@@@..@@OO@@^      
+        @@@O@@@@@..@@OO@@O.=@@@@..@@@@^......@@........=@@@O .@@@@*.=@@O@@@..@@OO@@^      
+        @@@@@ =@@....@@^...=OOOO .OOOO.................=OOO^ .OOOO....=@O....@@@@.        
+          =@@ =@@....O@....=OOOOOOOOOO.................=OOOOOOOOOO....=O/....@@@@^        
+                .@@@@O@^......\\\\\o......................\]\\/o......=O@@@@@@@           
+                     O@^.........................oo...................=@@O@@O@@    .      
+                 .  .@@^..................=@@oooO.....................=@@@@OO@@           
+               ..... ..@@O..............=@@.,@@.....................=@O[[.=@@..           
+                 .     ,[\@@@@........O@^.=@@...................@@@@/[`    [[             
+                         ,[[[\@@@@@@@@/[\@@@@@@@@@@@@@@@@@@@@@@@[[[[`        ,oo^         
+                             ,[[[[@@//\]/[[[[[[[[[[[[[[[[[[[[[[[.            ^            
+                                 .[[OO/[`                                                 
+                                    @@^ demo锛歨ttps: //www.nmmnm.co/showmsg/forumid/5/id/107796?page=1*/
+$(document).ready(function () {
+	$("body").append("<style>.group_0 {color: transparent !important;background: url(https://i.loli.net/2019/10/21/elorKWgY97UkxNv.png) no-repeat !important;}.title{background:transparent;text-align:center}.alert-danger {border-color: transparent; background: transparent;}.bg-info {background-color: transparent;}.am-topbar {background: transparent;}.am-topbar-btn {background-color: transparent;border-color: transparent;}.am-topbar-btn:hover {background-color: transparent;border-color: transparent;}.bg-info {background-color: transparent;}</style>")
+    decorateYeForFans();
+    var select = document.getElementById("duqian");
+    var add = document.createElement("p");
+    add.setAttribute("id", "yeDocarateDiv");
+    select.before(add);
+    add.innerHTML =
+        '<a href="javascript:;" Onclick="$(&apos;#yeColor&apos;).toggle()">鐐规垜瑁呮壆鏈笘</a><br> <form id="yeColor" style="display: none;">  娉ㄩ噴锛氬彉鑹插姛鑳戒腑锛屽埛鏂板悗涓€鍒囦細鎭㈠鍘熸牱锛岄櫎闈炵偣淇濆瓨鐒跺悗鍙戦€併€備繚瀛樺姛鑳芥祴璇曚腑銆�<a href="https://kuler.adobe.com" target="_blank">鑹插僵鎼厤鍙傝€冪偣鎴�</a><br>閫夋嫨棰滆壊锛� <input type="color" id="ye_color"><p>   <select id="ye_decorate_select"><option >閫夋嫨闇€瑕佹洿鏀圭殑鏍峰紡</option>     <option value="ye_whole">鐣岄潰鏁翠綋鍙樿壊</option> <option value="ye_title">鏍囬棰滆壊</option>         <option value="ye_banner_shadow">banner闃村奖棰滆壊</option>     <option value="ye_reply_font">鍥炲鏂囧瓧棰滆壊</option><br>     <option value="ye_name_time">鏄电О琛岄鑹�</option>  <option value="ye_block">鍒嗛殧绾块鑹�</option><br>     </select>  <button id="ye_decorate" type="button">棰勮</button><button id="ye_save_decorate" type="button">淇濆瓨</button></p> </form>';
+    $("#ye_decorate").click(decorateYeUnSave);
+    $("#ye_save_decorate").click(decorateYe);
+    $("#yeColor").prepend("<p id='YeBackgroundDiv'></p>");
+    $("#YeBackgroundDiv").html("閫夋嫨鍙舵ゼ鑳屾櫙锛�" + yeBackgoundImageSelect);
+    $("#yeBackgroundImage").change(function () {
+        $("#wenbenkuang").val(
+            $("#wenbenkuang").val() +
+            "<st" +
+            "yle>body{background-attachment: fixed;background-image: url(" +
+            $("#yeBackgroundImage").val() +
+            ");background-size:100%;}</s" +
+            "tyle>"
+        );
+    });
+});
+function decorateYeUnSave() {
+    var ye_decorate_where = $("#ye_decorate_select").val();
+    var ye_color = $("#ye_color").val();
+    if (
+        ye_decorate_where == "" ||
+        ye_decorate_where == null ||
+        ye_color == "" ||
+        ye_color == null
+    ) {
+        return false;
+    } else {
+        var color = ye_color;
+        if (ye_decorate_where == "ye_whole") {
+            $(document).ready(function () {
+                $("span").css("cssText", "color:" + color + " !important");
+                $(".alert").css("cssText", "color:" + color + " !important");
+                $(".footer").css("cssText", "color:" + color + " !important");
+                $(".glyphicon").css("cssText", "color:" + color + " !important");
+                $(".authorinfo").css("cssText", "color:" + color + " !important");
+                $("hr").css("cssText", "color:" + color + " !important");
+                $(".text-success").css("cssText", "color:" + color + " !important");
+                $(".alart").css("cssText", "color:" + color + " !important");
+                $(".alart.alart-danger").css(
+                    "cssText",
+                    "color:" + color + " !important"
+                );
+                $(".am-form-group").css("cssText", "color:" + color + " !important");
+                $(".form-group").css("cssText", "color:" + color + " !important");
+                $("#file").css("cssText", "color:" + color + " !important");
+                $(".pull-right span").css("cssText", "color:" + color + " !important");
+                $(".pull-right a").css("cssText", "color:" + color + " !important");
+                $(".bg-info span a").css("cssText", "color:" + color + " !important");
+                $(".form-group a").css("cssText", "color:" + color + " !important");
+                $(".am-topbar-brand a").css(
+                    "cssText",
+                    "color:" + color + " !important"
+                );
+                $("hr").css(
+                    "cssText",
+                    "background:" + color + " !important;margin-top: 2px"
+                );
+                $(".title").css("cssText", "color:" + color + " !important");
+                $(".title > font").css("cssText", "color:" + color + " !important");
+            });
+        } else if(ye_decorate_where=="ye_title"){
+        	$(".title").css("cssText", "color:" + color + " !important");
+        	$(".title > font").css("cssText", "color:" + color + " !important");
+        }else if (ye_decorate_where == "ye_banner_shadow") {
+            $(document).ready(function () {
+                $(".title").css("box-shadow", "0px 4px 7px " + color);
+            });
+        } else if (ye_decorate_where == "ye_reply_font") {
+            $(document).ready(function () {
+                $(".replybody").css("cssText", "color:" + color + " !important");
+            });
+        } else if (ye_decorate_where == "ye_name_time") {
+            $(document).ready(function () {
+                $(".authorinfo").css("cssText", "color:" + color + " !important");
+                $("div.authorinfo > div > span:nth-child(1)").css(
+                    "cssText",
+                    "color:" + color + " !important"
+                );
+                $(".group_0+span").css("cssText", "color:" + color + " !important");
+            });
+        } else if (ye_decorate_where == "ye_block") {
+            $(document).ready(function () {
+                $("hr").css(
+                    "cssText",
+                    "background:" + color + " !important;margin-top: 2px"
+                );
+            });
+        }
+    }
+}
+
+function decorateYe() {
+    var color_whole = $("span").css("color");
+    var save =
+        "<s" +
+        "tyle>span{color:" +
+        $("span").css("color") +
+        " !important;}a{color:" +
+        $("a").css("color") +
+        " !important;}.glyphicon{color:" +
+        $(".glyphicon").css("color") +
+        ";}.authorinfo{color:" +
+        $(".authorinfo").css("color") +
+        ";}div.authorinfo > div > span:nth-child(1){color:" +
+        $("div.authorinfo > div > span:nth-child(1)").css("color") +
+        " !important}.authorinfo > div > span:nth-child(3){color:" +
+        $(".group_0+span").css("color") +
+        " !important}hr{background :" +
+        $("hr").css("background") +
+        ";}.text-success{color:" +
+        $(".text-success").css("color") +
+        ";}.title{color:" +
+        $(".title").css("color") +
+        " !important;}.title>font{color:" +
+        $(".title").css("color") +
+        " !important;}.alert{color:" +
+        $(".alert").css("color") +
+        "}.footer{color:" +
+        $(".footer").css("color") +
+        " !important;}.am-form-group{color:" +
+        $(".am-form-group").css("color") +
+        "}.form-group{color:" +
+        $(".form-group").css("color") +
+        "}#file{color:" +
+        $("#file").css("color") +
+        "}.title{box-shadow:" +
+        $(".title").css("box-shadow") +
+        ";}.replybody{color:" +
+        $(".replybody").css("color") +
+        " !important;}</s" +
+        "tyle>";
+    $("#wenbenkuang").val($("#wenbenkuang").val() + save);
+}
+
+function decorateYeForFans() {}
+var yeBackgoundImageSelect =
+    '<select id="yeBackgroundImage"><option>閫夋嫨鑳屾櫙鍥剧墖</option><option value="https://i.loli.net/2019/09/20/z3TiaodfSY5ZXJw.jpg ">绾㈤緳鍙�</option>	<option value="https://i.loli.net/2019/10/04/98rEXRZtFuGI2vN.jpg">闈掗緳鍙�</option>	<option value="https://i.loli.net/2019/09/21/SCvun9LDIB8qEha.jpg">鏇插鍙�</option>	<option value="https://i.loli.net/2019/09/21/F3QVC8IY1A5qr9s.png">闈掗緳+鏇插鍙舵贩鍚�</option>	<option value="https://i.loli.net/2019/09/21/xmcJ3HrF1z6N57D.jpg">绾㈡棗鍙�</option>	<option value="https://i.loli.net/2019/09/23/jcQf1X5ZEP3AbTm.jpg">绾㈤緳+鐜嬪骇鎷兼帴</option>	<option value="https://i.loli.net/2019/09/21/Clo2gyIPHENhKZ9.jpg">铔嬬硶鍙�</option>	<option value="https://i.loli.net/2019/09/23/tYfVJiaUBrOl3Wv.jpg">钃濊壊鑳屾櫙鐚彾</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/723c569b09b8a042.jpg">鏉庡畞鍙�</option>	<option value="https://i.loli.net/2019/09/23/8X9duYs7UEbRHhj.jpg">绾㈡棗鍙�2</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/eccdfec8056f838d.jpg">宸﹀彾淇簳绾�+绾㈡棗鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/e2097b41ff85c4a1.jpg">鍙跺彾鍠濊厞鍏播</option>	<option value="https://i.loli.net/2019/09/24/mr5OTKXGpzDfxRj.jpg">蹇潯鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/3f2d2409d1e8a868.jpg">鎶辨灂鍙�</option>	<option value="https://i.loli.net/2019/09/25/6xgaeNk5bpZqWSc.jpg">绀肩墿鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/c97b0fed16f86299.jpg">璧风偣鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/d11d4ca3e01c3568.jpg">鐜嬮箯鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/b8fb6fc1a0d05b85.jpg">鐫¤鍠靛彾</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/09d423b757254003.jpg">瑗胯鍙�</option>	<option value="https://i.loli.net/2019/09/26/2hpZtzvHgyMYeq6.jpg">姝︿緺鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/21f9e77375bdf0c0.jpg">涓冨鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/c93a0432d2422dd1.jpg">绗簩瀛ｅ崼琛ｅ彾</option><option value="https://ftp.bmp.ovh/imgs/2019/10/9b984695f5061f67.jpg">涓囧湥鍙禥鐗�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/e36ec7b1a8f97583.jpg">鐢熸棩澹佺焊鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/dc7c14a3773104da.jpg">姘存墜鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/09/5d72233cd6d046de.jpg">闄ゅ鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/a2b6c21c78201382.jpg">鍥藉簡Q鐗�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/406bb9c36be319da.jpg">鐢熸棩浼氬弻鍙�</option>	<option value="https://i.loli.net/2019/10/01/DM4gmucvXIxhaEW.jpg">18涓鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/0da05a117a44c4c0.jpg">19骞村浗搴嗗彾</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/f34306cec019e1b7.jpg">HE鑽掗噹鎵撴€彾</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/4ba42c98702b2d19.jpg">鍏冨鍏斿厰鍙�</option>	<option value="https://i.loli.net/2019/10/05/zZRX5eUjGBThDka.jpg">鎷涜储鐚彾</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/dc50a5c8de236672.jpg">鎵т簨鍙剁涓€鐗�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/3958cba32cea7421.jpg">婊戦洩鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/c0059e76891da89c.jpg">鍗佸洓鐗堝悰鑾瑧</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/6cb2488a2d7e2f9a.jpg">绔崍绮藉彾</option>	<option value="https://i.loli.net/2019/10/19/bpwj2uYkWZRd1SE.jpg">鐚爲鐗堝叴娆ｅ彾</option>	<option value="https://i.loli.net/2019/10/11/X7EodrOKci2LATU.jpg">涓囧湥鍙舵姣旂増</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/4935d481c1fadca2.jpg">榫欐姮澶村彾</option>	<option value="https://i.loli.net/2019/10/14/ksVn1YfTFKNBeCX.jpg">绛夎韩鎶辨灂鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/cb838f203b079f1e.jpg">绮樺湡鍙�</option>	<option value="https://i.loli.net/2019/10/16/9bPEMSkVeyzwUrh.gif">鎺夎惤鐨勮捣鐐瑰彾</option>	<option value="https://i.loli.net/2019/10/17/kUIJ56aReiMYNf9.gif">wink鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/fb5f2d88521e95e3.jpg">濮滈ゼ鍙�</option>	<option value="https://ftp.bmp.ovh/imgs/2019/10/c689a7e40e9dce8d.jpg">鍙ゅ墤鑱斿姩鍙�</option>	<option value="https://i.loli.net/2019/12/24/i6tObdIT9xhf7ml.png">骞挎挱鍓у彾</option>  <option value="https://i.loli.net/2019/12/25/pz3jKhRfaXnD6OT.jpg">Q鐗堝湥璇炲彾</option> <option value="https://i.loli.net/2019/12/25/HMAJ5YR1yS8isg2.png">鍦ｈ癁姝ｆ瘮鍙跺ご鍍�</option><option value="杈撳叆鍥剧墖鐨刪ttp鍦板潃">鑷繁鏂板</option></select>';
