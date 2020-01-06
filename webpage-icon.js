0131278bc56bd9752b1ce56b9bc16d29b68d5b38
Aenigma-fishriver <57610861+Aenigma-fishriver@users.noreply.github.com>
2020-01-06 22:05:40 +0800

    Create webpage-icon.js

---
+++ webpage-icon.js
@@ -0,0 +1,4 @@
+var linkObject = document.createElement("link");
+linkObject.rel = "shortcut icon";
+linkObject.href = "https://i.loli.net/2019/11/10/6UpPSFLOTtzHs89.png";
+document.getElementsByTagName("head")[0].appendChild(linkObject);
